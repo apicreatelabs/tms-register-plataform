acb438fcce0efdd09a85e7b9c7842db96f429d5f
JUAN DAVID LOAIZA REYES <juan400reyesloaiza@gmail.com>
2021-02-01 16:36:10 -0500

    open port

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+require('dotenv').config();
+
+const app = express();
+
+let port  =  process.env.PORT || 3001;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(port,() =>{
+
+    console.log(`API running for port ${port}`);
+
+})
