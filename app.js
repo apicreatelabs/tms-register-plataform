cad3172f0f7e58e0d7cfe7f38963591fff42d171
Jorge Ivan Reyes Valencia <jorgeivanreyesvalencia92@gmail.com>
2021-02-02 14:43:47 -0500

    refactor

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { body, validationResult } = require('express-validator');
 
 require("dotenv").config();
 
