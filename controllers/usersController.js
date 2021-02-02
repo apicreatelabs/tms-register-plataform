7711418c4a708f61f67122615640ea5866f705b2
Jorge Ivan Reyes Valencia <jorgeivanreyesvalencia92@gmail.com>
2021-02-02 09:20:05 -0500

    controllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,12 +1,16 @@
 function register(req,res){
-
+    let data =  req.data
+    //metodos para registrar un usuario
 }
 
 function login(req,res){
-
+    let data =  req.data
+    //metodos para autenticar
 }
 
 function recoveryPassword(req,res){
+    let data =  req.data
+    //metodos para  recovery password
 }
 
 module.exports = {
